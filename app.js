b3dfeff357d76d7e542fdbb96ec6a6c0cc36194f
Logentheran Maratthandavar <logen@logen-mac.local>
2022-07-27 18:54:08 +0800

    First Cimmit

---
+++ app.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      courseGoalA: "Finish the course and learn Vue!",
+      courseGoalB: "Master Vue and build amazing apps!",
+      vueLink: "https://vuejs.org/",
+    };
+  },
+  methods: {
+    outputGoal() {
+      const randNum = Math.random();
+      return randNum < 0.5 ? this.courseGoalA : this.courseGoalB;
+    },
+  },
+});
+
+app.mount("#user-goal");
